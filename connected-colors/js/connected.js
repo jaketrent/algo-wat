21fab1f8b720f86c6e41a0fa583a83216d95602d
jaketrent <trent.jake@gmail.com>
2021-05-15 05:42:08 -0600

    connected colors iterative

--- connected-colors/js/connected.js
+++ connected-colors/js/connected.js
@@ -2,25 +2,47 @@ export const maxConnected = (grid) => {
   let maxN = 0
   for (const y in grid) {
     for (const x in grid[y]) {
-      maxN = max(maxN, dfs(grid, {}, parseInt(x, 10), parseInt(y, 10)))
+      maxN = max(
+        maxN,
+        traverseIterative(grid, {}, parseInt(x, 10), parseInt(y, 10))
+      )
     }
   }
   return maxN
 }
 
-const dfs = (grid, visited, x, y) => {
+const traverse = (grid, visited, x, y) => {
   const visitedKey = x + ',' + y
   if (visitedKey in visited) return 0
 
   visited[visitedKey] = true
   let result = 1
-  for (const neighbor of neighbors(grid, x, y)) {
-    result += dfs(grid, visited, neighbor[0], neighbor[1])
+  for (const neighbor of filterConnectedNeighbors(grid, x, y)) {
+    result += traverse(grid, visited, neighbor[0], neighbor[1])
   }
   return result
 }
 
-const neighbors = (grid, x, y) => {
+const traverseIterative = (grid, visited, x, y) => {
+  const stack = [[x, y]]
+
+  let result = 0
+  while (stack.length > 0) {
+    const [x, y] = stack.pop()
+
+    const key = x + ',' + y
+    if (key in visited) continue
+    visited[key] = true
+
+    result += 1
+    for (const neighbor of filterConnectedNeighbors(grid, x, y)) {
+      stack.push(neighbor)
+    }
+  }
+  return result
+}
+
+const filterConnectedNeighbors = (grid, x, y) => {
   const POSITIONS = [
     [0, 1],
     [1, 0],
