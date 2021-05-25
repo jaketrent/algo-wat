893f50023dc677a5e2414494efe60f674f5f0301
jaketrent <trent.jake@gmail.com>
2021-05-24 20:52:04 -0600

    from roman numeral in ruby

--- roman-numeral/ruby/lib/roman.rb
+++ roman-numeral/ruby/lib/roman.rb
@@ -1,4 +1,28 @@
 class Roman
+
+  def self.to(arabic)
+    return "" if arabic == 0
+
+    num, numeral = @@conversions.find { |entry| entry.first <= arabic }
+    numeral + to(arabic - num)
+  end
+
+  def self.from(roman, total=0)
+    return total if roman == ""
+
+    numeral = get_next_longest_sub_numeral roman
+    rest = roman[numeral.length..]
+    num, _numeral = @@conversions.find { |entry| entry.last === numeral }
+    from rest, total + num
+  end
+
+  def self.get_next_longest_sub_numeral(roman)
+    _num, numeral = @@conversions.find { |entry| roman == entry.last }
+    return numeral if numeral
+
+    self.get_next_longest_sub_numeral(roman[..-2])
+  end
+
   @@conversions = [
     [1000, "M"],
     [900, "CM"],
@@ -14,11 +38,6 @@ class Roman
     [1, "I"]
   ]
 
-  def self.to(arabic)
-    return "" if arabic == 0
-
-    num, numeral = @@conversions.find { |entry| entry.first <= arabic }
-    numeral + self.to(arabic - num)
-  end
+  private_class_method :get_next_longest_sub_numeral
 
 end
--- roman-numeral/ruby/spec/roman_spec.rb
+++ roman-numeral/ruby/spec/roman_spec.rb
@@ -3,30 +3,38 @@ require "spec_helper"
 require "roman"
 
 describe Roman do
-  describe '#to' do
-    cases = [
-      [0, ''],
-      [1, 'I'],
-      [2, 'II'],
-      [3, 'III'],
-      [4, 'IV'],
-      [5, 'V'],
-      [6, 'VI'],
-      [9, 'IX'],
-      [10, 'X'],
-      [20, 'XX'],
-      [49, 'XLIX'],
-      [50, 'L'],
-      [90, 'XC'],
-      [100, 'C'],
-      [500, 'D'],
-      [900, 'CM'],
-      [1000, 'M'],
-    ]
+  cases = [
+    [0, ''],
+    [1, 'I'],
+    [2, 'II'],
+    [3, 'III'],
+    [4, 'IV'],
+    [5, 'V'],
+    [6, 'VI'],
+    [9, 'IX'],
+    [10, 'X'],
+    [20, 'XX'],
+    [49, 'XLIX'],
+    [50, 'L'],
+    [90, 'XC'],
+    [100, 'C'],
+    [500, 'D'],
+    [900, 'CM'],
+    [1000, 'M'],
+  ]
 
+  describe '#to' do
     cases.each do |input, expected|
       it "converts #{input} to #{expected}" do
         expect(Roman.to(input)).to eq(expected)
+      end
+    end
+  end
+
+  describe '#from' do
+    cases.each do |expected, input|
+      it "converts #{input} to #{expected}" do
+        expect(Roman.from(input)).to eq(expected)
       end
     end
   end
